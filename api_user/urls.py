02a2b2d86f0593f9e2ae51c21552c3b40987fae5
TakuoIshida <archersparadox.100@gmail.com>
2020-08-23 15:50:05 +0900

    .env設定　gitignore設定　localhost動作確認

---
+++ api_user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import DefaultRouter
+
+app_name = 'user'
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path(''), include(router.urls)
+]
---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken import veiws
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('authen/', views.obtain_auth_token),
+    path('api/user/', include('api_user,urls')),
+    path('api/dm', include('api_dm,urls')),
+]
+
+urlpatterns += static(setthing.MEDIA_URL, document=settings.MEDIA_ROOT)
