7bce24b09455181266f50279f9d3783a321e2820
TakuoIshida <archersparadox.100@gmail.com>
2020-08-23 19:43:22 +0900

    api_use.views

--- api_user/views.py
+++ api_user/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import generics, authentication, permissions
+from api_user import serializers
+from core.models import Profile, FriendRequest
+from django.db.models import Q
+from rest_framework import viewsets
+from rest_framework import ValidationError
+from rest_framework.response import Response
 
-# Create your views here.
+
+# TODO import ↓内容を復習
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    queryset = FriendRequest.objects.all()
+    serializer_class = serializers.FriendRequestSerializer
+    authentication = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(Q(ask_to=self.request.user) | Q(ask_from=self.queryset.user))
+
+    def perform_create(self, serializer):
+        try:
+            serializer.save(ask_from=self.request.user)
+        except:
+            raise ValidationError('User can have only unique request')
+
+    def destroy(self, request, *args, **kwwargs):
+        response = {'message': 'Delete is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwwargs):
+        response = {'message': 'Patch is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
