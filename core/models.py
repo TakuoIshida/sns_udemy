0cf1d3c5e2792368d6e5a759957806e23f1df02e
TakuoIshida <archersparadox.100@gmail.com>
2020-08-23 17:16:08 +0900

    admin作成

--- core/models.py
+++ core/models.py
@@ -35,8 +35,8 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
 
     email= models.EmailField(max_length=50, unique=True)
-    is_staff = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
