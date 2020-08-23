c33f59e2e11a68da8a3447460b591f1848ee1a1a
TakuoIshida <archersparadox.100@gmail.com>
2020-08-23 17:56:18 +0900

    タイポ修正

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import veiws
+from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
 
