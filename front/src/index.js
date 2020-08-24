a8b2e7281d0b8ea77c21332319a29afab1e83f57
TakuoIshida <archersparadox.100@gmail.com>
2020-08-24 21:01:01 +0900

    serviceworker　設定

--- front/src/index.js
+++ front/src/index.js
@@ -11,4 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// TODO: PWA機能（オフラインでも、キャッシュによってWebページが見れる機能）がデフォルトで無効化されている
 serviceWorker.unregister();
+// PWA有効化　（開発中はキャッシュされると更新内容の確認が大変になるので無効化する）
+// serviceWorker.register();
