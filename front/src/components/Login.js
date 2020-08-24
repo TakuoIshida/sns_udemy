8cee63e1bb535a23129b68cf5e053202a51c832d
TakuoIshida <archersparadox.100@gmail.com>
2020-08-25 00:40:29 +0900

    build error解消

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,14 +1,15 @@
-import React, { userReducer } from 'react';
-import { withCookies } from 'react-cookie';
-import axios from "axios";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
+// import React, { userReducer } from 'react';
+// import { withCookies } from 'react-cookie';
+// import axios from "axios";
+// import Avatar from '@material-ui/core/Avatar';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import Typography from '@material-ui/core/Typography';
+// import Container from '@material-ui/core/Container';
+// import CircularProgress from '@material-ui/core/CircularProgress'
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import CircularProgress from '@material-ui/core/CircularProgress'
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -46,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
 const Login = () => {
     const classes = useStyles()
     return (
-        <div>
+        <div className={classes.avatar}>
             here is navbar
         </div>
     )
